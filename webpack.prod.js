68c440c1ebebe3b56bb4fd13dbf499730693e6d6
nawaf <princenawaf99@hotmail.com>
2020-04-26 04:11:16 +0300

    final

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,40 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-
+const MiniCssExtractPlugins = require('mini-css-extract-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin');
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    devtool: 'source-map',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugins.loader, 'css-loader', 'sass-loader']
             }
+
         ]
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugins({filename: '[name].css'}),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
